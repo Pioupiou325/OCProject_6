3e1da8a2e9b62f3bd8112cb880914a6f63345dc2
Pioupiou325 <127089710+Pioupiou325@users.noreply.github.com>
2023-10-28 16:46:49 +0200

    formlogement pics ok

--- src/pages/Formlogement.js
+++ src/pages/Formlogement.js
@@ -1,13 +1,26 @@
 import React from "react";
 import logements from "../datas/logements.json";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
+import Banner from "../components/Banner";
 
 function Formlogement() {
   const { id } = useParams();
-  const logement = logements.find(item => item.id === id);
-  return <div>
-    <img src={logement.host.picture} alt="propriétaire"></img>
-  </div>;
-};
+  const logement = logements.find((item) => item.id === id);
+  const picture = logement.pictures;
+  const pictures_array = [];
+  picture.forEach(element => {
+    ;
+    pictures_array.push(<img key={element.position} src={element} alt="collection"></img>);
+    
+      });
+  return (
+    <div>
+      <Banner />
+      {pictures_array}
+      
+      <img src={logement.host.picture} alt="propriétaire"></img>
+    </div>
+  );
+}
 
 export default Formlogement;
