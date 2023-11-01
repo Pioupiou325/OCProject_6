3e8da71ea9731305dd60cc2e302107369ae7a1e5
Pioupiou325 <127089710+Pioupiou325@users.noreply.github.com>
2023-11-02 00:27:44 +0100

    redirection id false ok

--- src/pages/Formlogement.js
+++ src/pages/Formlogement.js
@@ -1,6 +1,6 @@
 import React from "react";
 import logements from "../datas/logements.json";
-import { Navigate, useParams} from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 import Banner from "../components/Banner";
 import Footer from "../components/Footer";
 import Carrousel from "../components/Carrousel";
@@ -11,30 +11,33 @@ import Collapse from "../components/Collapse";
 function Formlogement() {
   const { id } = useParams();
   const logement = logements.find((item) => item.id === id);
-  
+
   if (logement === undefined) {
-    
-    return Navigate( `/error`);
-  }
-  else {
-    
-  
-  
+    return <Navigate to="/*" />;
+  } else {
     return (
       <div className="formlogement">
         <Banner />
         <Carrousel pics={logement} />
-        
+
         <p className="title">{logement.title}</p>
         <p className="subTitle">{logement.location}</p>
-          <Tag className="tags" tags={logement.tags} />
+        <Tag className="tags" tags={logement.tags} />
         <div className="containerHost">
           <p className="name">{logement.host.name}</p>
-          <img className="host"src={logement.host.picture} alt="propriétaire"></img>
+          <img
+            className="host"
+            src={logement.host.picture}
+            alt="propriétaire"
+          ></img>
         </div>
         <Rating className="rating" stars={logement.rating} />
-        <Collapse title="Description" content={logement.equipments} size="medium" />
-        
+        <Collapse
+          title="Description"
+          content={logement.equipments}
+          size="medium"
+        />
+
         {/* <Footer /> */}
         {/* minheight100vh */}
       </div>
