7793aa01b63106f8e370f4cd2fba817f134cb77d
Pioupiou325 <127089710+Pioupiou325@users.noreply.github.com>
2023-11-06 22:38:43 +0100

    content for collapse ok

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,47 @@
 import React from "react";
+import Collapse from "../components/Collapse";
 import PhotoBanner from "../components/PhotoBanner";
 import Banner from "../components/Banner";
 import Footer from "../components/Footer";
-
+import logements from "../datas/logements.json";
+console.log(logements);
 const About = () => {
   return (
     <div className="about">
       <Banner />
       <PhotoBanner src="/assets/montagnes_apropos.jpg" alt="montagnes" />
+      <div className="listCollapseAbout">
+        <Collapse
+          className="collapseInForm"
+          title="Fiabilité"
+          content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont
+            régulièrement vérifiées par nos équipes."
+          size="medium"
+        />
+        <Collapse
+          className="collapseInForm"
+          title="Respect"
+          content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
+            perturbation du voisinage entraînera une exclusion de notre plateforme."
+          size="medium"
+        />
+        <Collapse
+          className="collapseInForm"
+          title="Service"
+          content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
+            perturbation du voisinage entraînera une exclusion de notre plateforme."
+          size="medium"
+        />
+        <Collapse
+          className="collapseInForm"
+          title="Sécurité"
+          content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement
+            correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au
+            locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons
+            également des ateliers sur la sécurité domestique pour nos hôtes."
+          size="medium"
+        />
+      </div>
       <Footer />
     </div>
   );
