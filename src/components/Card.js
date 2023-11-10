94536b0e83683eef971061939c91eb5fe17dc882
Pioupiou325 <127089710+Pioupiou325@users.noreply.github.com>
2023-11-10 14:56:19 +0100

    modif alt cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,19 +2,17 @@ import React from "react";
 import logements from "../datas/logements.json";
 import { NavLink } from "react-router-dom";
 function Card() {
-  const  list_logements = logements.map((logement) =>  logement);
+  const list_logements = logements.map((logement) => logement);
 
   return (
     <div className="card">
       <ul>
         {list_logements.map((logement) => (
           <NavLink key={logement.id} to={`/formlogement/${logement.id}`}>
-            <li >
+            <li>
               <div className="logement">
-                <img src={logement.cover} alt={`cover ${logement.title}`} />
+                <img src={logement.cover} alt={`${logement.title}`} />
                 <div className="overlay"></div>
-
-
                 <p>{logement.title}</p>
               </div>
             </li>
